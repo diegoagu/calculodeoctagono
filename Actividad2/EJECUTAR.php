1a22edf3e37ee761c08f5710e54ffbaf65258f7c
diegoagu <72635507+diegoagu@users.noreply.github.com>
2021-02-11 19:00:16 -0400

    Add files via upload

---
+++ Actividad2/EJECUTAR.php
@@ -0,0 +1,16 @@
+<?php 
+
+$P = $_POST['P'];
+$AP = $_POST['AP'];
+$mensaje = "El area del Octagono es: ";
+$Perimetro;
+$APotema;
+
+$area = (($P) * ($AP))/2;
+
+echo"$mensaje $area";
+
+echo"<a href='datos.html'>Regresar</a>";
+
+
+?>
